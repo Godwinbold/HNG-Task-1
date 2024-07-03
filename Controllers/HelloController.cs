2edae748293e83211289f0e8d8bc57885c0ddd45
Godwinbold <gordonblaze5@gmail.com>
2024-07-03 09:30:39 +0100

    HNG-Task-1

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Hello_HNG_API_.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class HelloController : ControllerBase
+	{
+		
+			private readonly IHttpClientFactory _httpClientFactory;
+
+			public HelloController (IHttpClientFactory httpClientFactory)
+			{
+				_httpClientFactory = httpClientFactory;
+			}
+
+			[HttpGet]
+			public async Task<IActionResult> Get([FromQuery] string visitor_name)
+			{
+				string clientIp = HttpContext.Connection.RemoteIpAddress?.ToString();
+				string location = "New York";
+				double temperature = 11;
+
+				var response = new
+				{
+					client_ip = clientIp,
+					location = location,
+					greeting = $"Hello, {visitor_name}!, the temperature is {temperature} degrees Celsius in {location}"
+				};
+
+				return Ok(response);
+			}
+		
+	}
+}
---
+++ HelloController.cs
@@ -0,0 +1,14 @@
+namespace Hello_HNG_API_
+{
+	public class HelloController
+
+	{
+		public DateTime Date { get; set; }
+
+		public int TemperatureC { get; set; }
+
+		public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+		public string? Summary { get; set; }
+	}
+}
